b22bbcad02790d7fd3fb258e2d626935ba93df73
Felipe Sokulski <felipe.ksokulski@gmail.com>
2026-08-10 17:49:55 -0300

    adicionando seção: Sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
  import Header from './components/Header'
  import Hero from './components/Hero'
+ import Sobre from './components/Sobre'
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Hero></Hero>
+      <Sobre></Sobre>
     </div>
       
   ) 
