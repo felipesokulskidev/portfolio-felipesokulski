e1df57b26d06a0d36fb45ba94a7984aa3066bd8f
Felipe Sokulski <felipe.ksokulski@gmail.com>
2026-08-13 20:02:32 -0300

    deixando responsivo

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,19 +1,24 @@
 import './Contato.css'
+import { Mail, SquareArrowOutUpRight } from 'lucide-react';
+
 
 function Contato(){
     return(
         <section className='contato' id="contato">
             <span className='contato-title'>Vamos conversar!</span>
             <span className='contato-desc'>Estou aberto à novas oportunidades, novos projetos e parcerias. Buscando um estágio na área da tecnologia.</span>
-            <div className='contato-links'>
-                <a href="mailto:felipe.ksokulski@gmail.com?subject=Contato via portfólio&body=Olá Felipe, vim através do seu portfólio!">
+            <div className="contato-links">
+                <a href="mailto:felipe.ksokulski@gmail.com?subject=Contato via portfólio&body=Olá Felipe, vim através do seu portfólio!" className="contato-links-item">
+                    <Mail size={16} />
                     Enviar e-mail
                 </a>
-                <a href="http://linkedin.com/in/felipekrupasokulski">
+                <a href="http://linkedin.com/in/felipekrupasokulski" target="_blank" rel="noopener noreferrer" className="contato-links-item">
                     LinkedIn
+                    <SquareArrowOutUpRight size={16} />
                 </a>
-                <a href="https://github.com/felipesokulskidev/">
+                <a href="https://github.com/felipesokulskidev/" target="_blank" rel="noopener noreferrer" className="contato-links-item">
                     GitHub
+                    <SquareArrowOutUpRight size={16} />
                 </a>
             </div>
         </section>
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,5 +1,6 @@
 import './Sobre.css'
 import pucLogo from '../assets/pucpr-logo.png';
+import cpmLogo from '../assets/cpmpr-logo.png';
 
 function Sobre(){
     return(
@@ -7,19 +8,28 @@ function Sobre(){
             <span className='sobre-title'>Sobre mim</span>
             <span className='sobre-text'> Ao longo da minha trajetória acadêmica e pessoal, desenvolvi <span className='sobre-text-span'>alta facilidade de aprendizado</span> para assimilar rapidamente novas tecnologias e conceitos, demonstrando <span className='sobre-text-span'>adaptabilidade diante de diferentes cenários,</span> aliada à proatividade para antecipar necessidades, sugerir soluções e contribuir com melhorias contínuas nos processos, com um perfil que <span className='sobre-text-span'>busca constantemente novos conhecimentos de forma independente</span> e os aplica de maneira prática em projetos, fortalecendo ainda mais a capacidade de crescimento e evolução dentro das equipes.</span>
             <span className='formacao-title'>Formação</span>
-            <div className='formacao'>
-                
-                <div className='formacao-logo-puc'>
-                    <img src={pucLogo} alt="" />
+            <div className="formacao-lista">
+                <div className="formacao">
+                    <div className="formacao-logo-puc">
+                        <img src={pucLogo} alt="PUCPR" />
+                    </div>
+                    <div className="formacao-texto">
+                        <span className="formacao-faculdade">Ciência da Computação</span>
+                        <span className="formacao-local">Pontifícia Universidade Católica do Paraná</span>
+                        <span className="formacao-conclusao">Jan 2025 - Dez 2028 • 4°/8° semestre</span>
+                    </div>
                 </div>
-                
-                <div className='formacao-texto'>
-                    
-                    <span className='formacao-faculdade'>Ciência da Computação</span>
-                    <span className='formacao-local'>Pontifícia Universidade Católica do Paraná </span>
-                    <span className='formacao-conclusao'>Jan 2025 - Dez 2028 • 4°/8° semestre</span>
+
+                <div className="formacao">
+                    <div className="formacao-logo-puc">
+                        <img src={cpmLogo} alt="Colégio da Polícia Militar" />
+                    </div>
+                    <div className="formacao-texto">
+                        <span className="formacao-faculdade">Ensino Médio</span>
+                        <span className="formacao-local">Colégio da Polícia Militar do Paraná</span>
+                        <span className="formacao-conclusao">Jan 2022 - Dez 2024</span>
+                    </div>
                 </div>
-                
             </div>
 
             <span className='stack-title'>Ferramentas</span>
